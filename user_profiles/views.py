ca6f65741e3021cbabcd780c95e8404652eeea19
Hemanth Kumar <hemanth7787@gmail.com>
2015-12-07 15:15:31 +0530

    global notification messages

--- user_profiles/views.py
+++ user_profiles/views.py
@@ -1,25 +1,30 @@
-from django.shortcuts import render
-from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import render, redirect
+from django.contrib import  auth, messages
 from django.views.generic import View
 from django import http
+
 from forms import LoginForm
 
 class UserAuthentication(View):
     def get(self, request, *args, **kwargs):
         if request.GET.get('action', '') == "logout":
-            logout(request)
+            auth.logout(request)
+            messages.info(request, 'Logout successful..')
         return  render(request, "user_profiles/login.html", {
     		"form": LoginForm,
             })
     def post(self, request, *args, **kwargs):
         username = request.POST['username']
         password = request.POST['password']
-        user = authenticate(username=username, password=password)
+        user = auth.authenticate(username=username, password=password)
         if user is not None:
             if user.is_active:
-                login(request, user)
-                return http.HttpResponse( "Login success.. ")
+                auth.login(request, user)
+                messages.success(request, 'Login successful..')
+                return redirect('home')
             else:
-                return http.HttpResponse( "Acount not active.. ")
+                messages.success(request, 'Acount not active..')
+                return redirect('login')
         else:
-            return http.HttpResponse( "Err.. ")
+            messages.error(request, "Login failed!..")
+            return redirect('login')

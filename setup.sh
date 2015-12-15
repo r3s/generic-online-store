8355f0400fe498d3e6be5e8f83ba95888bb12792
Hemanth Kumar <hemanth7787@gmail.com>
2015-12-15 12:38:10 +0530

    Setup script updated

--- setup.sh
+++ setup.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
+# Tested on ubuntu 14.04
+# Install OS libraries
 sudo apt-get install python-dev python-setuptools libjpeg62-dev zlib1g-dev libfreetype6-dev python-virtualenv build-essential
+# Create python virtual enviorment
 virtualenv env
+# Activate virtual enviorment
 source env/bin/activate
+# Install python libraries in virtual environment
+pip install -r requirements.txt
+# Create database and admin user
 python manage.py syncdb
+# Run development server
 python manage.py runserver
-

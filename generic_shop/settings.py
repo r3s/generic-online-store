77eb1e6bc59968831367c9fb78a1e19d37603fca
Hemanth Kumar <hemanth7787@gmail.com>
2016-01-14 00:35:57 +0530

    Thumbnail plugin added

--- generic_shop/settings.py
+++ generic_shop/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # 3rd party libraries
+    'sorl.thumbnail',
     # User apps
     'user_profiles',
     'gs_core',
